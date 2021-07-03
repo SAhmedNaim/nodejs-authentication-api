fe714872a4f9db3dfde40868609e6da0ae7d5a00
unknown <naim.ahmed035@gmail.com>
2021-07-03 18:50:54 +0600

    Login User

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,15 +3,40 @@ import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 
 /**
- * Register User
+ * @desc Login User
+ * @route POST /api/users/login
+ * @access Public
+ */
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+  
+    if (user && (await user.matchPassword(password))) {
+        return res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid Email or Password');
+    }
+});
+
+/**
+ * @desc Register User
+ * @route POST /api/users/register
+ * @access Public
  */
 const register = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
     const userExist = await User.findOne({ email });
   
     if (userExist) {
-      res.status(400);
-      throw new Error('User already Exist');
+        res.status(400);
+        throw new Error('User already Exist');
     }
   
     const user = await User.create({ name, email, password });
@@ -30,9 +55,14 @@ const register = asyncHandler(async (req, res) => {
     }
 });
 
+/**
+ * @desc Fetch User
+ * @route GET /api/users
+ * @access Public
+ */
 const getUsers = asyncHandler(async (req, res) => {
     const users = await User.find({});
     res.json(users);
 });
 
-export { register, getUsers };
+export { login, register, getUsers };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { register, getUsers } from '../controllers/userController.js';
+import { login, register, getUsers } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/login', login);
 router.get('/', getUsers);
 
 export default router;
