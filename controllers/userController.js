6f576333e52e628d1088246fb12170caceb3ccf3
unknown <naim.ahmed035@gmail.com>
2021-07-03 18:27:07 +0600

    Register User

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,38 @@
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
+
+/**
+ * Register User
+ */
+const register = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const userExist = await User.findOne({ email });
+  
+    if (userExist) {
+      res.status(400);
+      throw new Error('User already Exist');
+    }
+  
+    const user = await User.create({ name, email, password });
+  
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    } else {
+      res.status(400);
+      throw new Error('Invalid User Data');
+    }
+});
 
 const getUsers = asyncHandler(async (req, res) => {
     const users = await User.find({});
     res.json(users);
 });
 
-export { getUsers };
+export { register, getUsers };
